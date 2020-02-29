8ae5510fa86562dab3a8f5d1c0e9e386fc6f991c
Mike Sinnema <msinnema33@gmail.com>
2020-02-28 18:39:54 -0700

    working on cra and file structure

--- expatjournal/src/App.js
+++ expatjournal/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import './App.css';
+import Countdown from './components/Countdown';
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to Expat Journal Team 2</h1>
+     <h2>We will be live in:</h2>
+     {/* <Countdown 
+		timeTillDate="03 06 2020, 9:00 am" 
+		timeFormat="MM DD YYYY, h:mm a" 
+	/> */}
     </div>
   );
 }
