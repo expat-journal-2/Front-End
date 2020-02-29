c9e1f30a6bc7377d3ef91eec951a918882cfbfe3
Mike Sinnema <msinnema33@gmail.com>
2020-02-28 20:37:22 -0700

    updated PrivateRoute and custom hook useForm

---
+++ expatjournal/src/components/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    return (
+        <Route
+          {...rest}
+          render = {() => {
+              return window.localStorage.getItem('token') ?
+              (<Component />) : (<Redirect to = 'sign-in' />);
+          }}
+        />  
+    );
+}
+
+export default PrivateRoute;
