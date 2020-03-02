31205ce63867ae348d838d4d5a42a6f4dd23add1
Mike Sinnema <msinnema33@gmail.com>
2020-03-02 15:24:20 -0700

    added sign in/up links to app.js and added background

--- expatjournal/src/App.js
+++ expatjournal/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 
 import './App.css';
@@ -16,12 +16,14 @@ const StyledApp = styled.div`
   background-image: url(${main});
   background-size: cover;
   height: 100vh;
+  opacity: 0.8;
 
   .main-btn {
     border: 2px solid white;
     border-radius: 5px;
     padding: 1% 2%;
     margin: 2%;
+    cursor: pointer;
   }
   `;
 
@@ -31,9 +33,12 @@ function App() {
     <div className="App">
      <h1>Welcome to Expat Journal Team 2</h1>
      <h2>We will be live Friday 6 March 2020</h2>
-
+      <Link to='/sign-up'>
      <button className='main-btn'>Sign up</button> 
+     </Link>
+     <Link to='/sign-in'> 
      <button className='main-btn'>Sign In</button>
+     </Link>
      <Router>
      <Switch>
        <Route exact path='/sign-in' component={Login} />
