89a0bcdc1e2feb3bc8ab97dde5753272952b4e09
Mike Sinnema <msinnema33@gmail.com>
2020-03-03 14:03:29 -0700

    working on editForm need work on handlechange and redux

--- expatjournal/src/App.js
+++ expatjournal/src/App.js
@@ -44,13 +44,7 @@ function App() {
      <Switch>
        <Route exact path='/sign-in' component={Login} />
        <Route exact path='/sign-up' component={SignUp}/>
-
-
-       
      </Switch>
-   
-
-
      </Router>
     </div>
     <Route path='/' component={Footer} /> 
