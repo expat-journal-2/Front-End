9f69744355b06de82e0158a70b140a208274fa6f
Mike Sinnema <msinnema33@gmail.com>
2020-03-03 13:59:05 -0700

    working on editForm - need to work on handlers and redux

--- expatjournal/src/components/EditForm.js
+++ expatjournal/src/components/EditForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { axiosWithAuth } from '../Utils/axiosWithAuth';
 
 const initialItem = {
     name: '',
@@ -11,3 +12,77 @@ const initialItem = {
     date: '',
 }
 
+const EditForm = props => {
+    const [ item, setItem ] = useState(initialItem);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const itemToUpdate = props.items.find(thing => `${thing.id}`=== id);
+        if(itemToUpdate) {
+            setItem(itemToUpdate);
+        }
+    }, [props.items, id]);
+
+//     const changeHandler = e => {
+//         e.persist();
+        
+//     }
+    const handleSubmit = e => {
+        axiosWithAuth()
+        .put(`/api/stories/${id}`, item)
+        .then(res => {
+            props.setItem(res.data);
+            props.history.push(`/api/stories/${id}`);
+        })
+        .catch(err => console.log(err));
+    };
+    
+    return (
+        <div>
+          <h2>Update Movie</h2>
+          <form onSubmit={handleSubmit}>
+          <label>Title</label>
+            <input 
+            type='text' 
+            id='name'
+            name='name'
+            required
+            value={formInputs.name || ''}
+            onChange={handleChanges}
+            placeholder="Enter a title"
+            />
+
+          <label>City/Town, State, Country</label>
+            <input 
+            type='text' 
+            id='location'
+            name='location'
+            value={formInputs.location || ''}
+            onChange={handleChanges}
+            placeholder="Enter a location"
+            />
+
+          <label>Choose an date for this trip</label>
+            <input 
+            type='text' 
+            id='date'
+            name='date'
+            value={formInputs.date || ''}
+            onChange={handleChanges}
+            placeholder="Enter an date"
+            />
+
+          <label>Your amazing story goes here</label>
+            <textarea 
+            type='text' 
+            id='content'
+            name='content'
+            required
+            value={formInputs.content || ''}
+            onChange={handleChanges}
+            placeholder="Enter your story"
+            />
+          </form>  
+        </div>
+    )
+}
